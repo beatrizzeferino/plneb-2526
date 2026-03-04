654c58764a9d1327cff00d5e481bc6b850868d60
Beatriz Zeferino <anabiazeferino@gmail.com>
2026-03-04 19:04:02 +0000

    Correção do script

--- TPC3/TPC3.py
+++ TPC3/TPC3.py
@@ -5,7 +5,7 @@ f=open("dicionario_medico.txt", "r", encoding="utf8")
 texto = f.read()
 
 # Passo 1: Remoção das quebras de página
-texto = re.sub(r'\f', '', texto) # Remove quebras de página físicas
+texto = re.sub(r'\f', '', texto)
 
 # Passo 2: Remoção das quebras de linhas nas descrições
 texto = re.sub(r'\n([a-zà-ú])', r' \1', texto)
@@ -16,7 +16,7 @@ texto = re.sub(r'\n\s*\n', '\n', texto)
 # Passo 4: Marcação dos termos com @
 texto = re.sub(r'\n([^A-Z\n]+)\n([A-ZÀ-Ú])', r'\n\n@\1\n\2', texto)
 
-# --- PARTE 2: CRIAR O DICIONÁRIO ---
+# Criação do dicionário
 conceitos = re.split(r'\n\n@', texto)
 conceitos_dict = {}
 
@@ -32,8 +32,7 @@ for conceito in conceitos[1:]:
             descricao = partes[1].replace('\n', ' ').strip()
             conceitos_dict[designacao] = descricao
 
-print(f"Sucesso! {len(conceitos_dict)} termos processados.")
-
+# Criação do ficheiro JSON
 def escreve_json(dict, filename):
     ficheiro=open(filename, "w",  encoding="utf8")
     json.dump(dict, ficheiro, indent=4, ensure_ascii=False)
